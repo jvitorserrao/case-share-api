95b0a4369a177419e06190089f39bb1fb7006a97
99039821 <joao.lopes@sop.ce.gov.br>
2025-07-25 09:13:14 -0300

    feat: update

--- src/main/java/com/share/case_jr/api/config/WebClientConfig.java
+++ src/main/java/com/share/case_jr/api/config/WebClientConfig.java
@@ -12,12 +12,19 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder
-                .codecs(configurer ->
-                        configurer.defaultCodecs().maxInMemorySize(1024 * 1024)
-                )
-                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0")
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.TEXT_HTML_VALUE)
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024))
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+                .defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7")
+                .defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br")
+                .defaultHeader(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+                .defaultHeader(HttpHeaders.CONNECTION, "keep-alive")
+                .defaultHeader("Sec-Fetch-Dest", "document")
+                .defaultHeader("Sec-Fetch-Mode", "navigate")
+                .defaultHeader("Sec-Fetch-Site", "none")
+                .defaultHeader("Sec-Fetch-User", "?1")
+                .defaultHeader("Upgrade-Insecure-Requests", "1")
                 .build();
     }
+
 
 }
